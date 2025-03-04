a6a1c25776b4fb6e4c53b300526194b25b091459
LeahBasson <leahbasson54@gmail.com>
2025-03-05 00:20:43 +0200

    :tada:

---
+++ ConversionKmsToMs/src/conversionkmstoms/ConversionKmsToMs.java
@@ -0,0 +1,15 @@
+
+package conversionkmstoms;
+
+
+public class ConversionKmsToMs {
+
+    
+    public static void main(String[] args) {
+        double kms = 4;
+        double miles = kms*0.621371;
+        
+        System.out.println("Miles: " + miles);
+    }
+    
+}
